79c26c3956f38c7f9bd6504800366789a5be2fee
Murillo Filho <murillohard@hotmail.com>
2018-03-15 22:38:38 -0300

    Add project files.

---
+++ SP.Minibank.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SP.Minibank.Infra
+{
+    public class Class1
+    {
+    }
+}
---
+++ SP.Minibank.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SP.Minibank.Shared
+{
+    public class Class1
+    {
+    }
+}
