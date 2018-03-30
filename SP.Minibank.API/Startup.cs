84f98f89a802893b099223a3e98f43b3bd37cb11
Murillo Filho <murillohard@hotmail.com>
2018-03-29 23:50:00 -0300

    #11 - Criação da classe EmailService e Implementação da IoC

--- SP.Minibank.API/Startup.cs
+++ SP.Minibank.API/Startup.cs
@@ -2,6 +2,11 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using SP.Minibank.Domain.Repositories;
+using SP.Minibank.Domain.Services;
+using SP.Minibank.Infra.MinibankContext.DataContexts;
+using SP.Minibank.Infra.MinibankContext.Repositories;
+using SP.Minibank.Infra.MinibankContext.Services;
 
 namespace SP.Minibank.API
 {
@@ -12,6 +17,12 @@ namespace SP.Minibank.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            
+            services.AddScoped<DataContext, DataContext>();
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+            services.AddTransient<IEmailService, EmailService>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
