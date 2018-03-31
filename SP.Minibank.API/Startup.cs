248c78ec41e71be61cc4deecb2ae963d2407a9f3
Murillo Filho <murillohard@hotmail.com>
2018-03-30 23:15:11 -0300

    #14 - Criação do arquivo de Configuração

--- SP.Minibank.API/Startup.cs
+++ SP.Minibank.API/Startup.cs
@@ -8,15 +8,27 @@ using SP.Minibank.Domain.Services;
 using SP.Minibank.Infra.MinibankContext.DataContexts;
 using SP.Minibank.Infra.MinibankContext.Repositories;
 using SP.Minibank.Infra.MinibankContext.Services;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using SP.Minibank.Shared;
 
 namespace SP.Minibank.API
 {
     public class Startup
     {
+        public static IConfiguration Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+
             services.AddMvc();
 
             services.AddResponseCompression();
@@ -26,6 +38,9 @@ namespace SP.Minibank.API
             services.AddTransient<IEmailService, EmailService>();
             services.AddTransient<CustomerHandler, CustomerHandler>();
             
+
+            Settings.ConnectionString = $"{Configuration["connectionString"]}";
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
